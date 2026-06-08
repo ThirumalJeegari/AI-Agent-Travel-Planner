d5b5a29e0a835f83f2a4e5923ef53337c6362855
ThirumalJeegari <thirumaljeegari21@gmail.com>
2026-06-08 22:49:31 +0530

    Initial commit

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-BACKEND_URL = "http://127.0.0.1:8000"
+BACKEND_URL = "https://ai-agent-travel-planner.onrender.com"
 
 st.set_page_config(
     page_title="AI Travel Planner",
